f0ddc14979706435a93c965673b6eae144ced304
Magooch <crvallecito@gmail.com>
2025-07-28 18:17:32 -0600

    Create method to display Expense Tracker Menu 'displayMenu()'

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
 public class Main {
     
+    // Displays Expense Tracker Console Menu
+    public static void displayMenu() {
+        System.out.println("Welcome To The Simple Expense Tracker!\n");
+        System.out.println("1. Manage Expenses");
+        System.out.println("2. View Expenses");
+        System.out.println("3. Calculate Total Expenses");
+        System.out.println("\"Menu\" to display menu");
+        System.out.println("\"Exit\" to exit\n");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Main Expense Tracker Console Menu Functionality
+        displayMenu();
     }
 }
